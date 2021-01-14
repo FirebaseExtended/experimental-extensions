aea98fca218eaf2bd545b48fcd5fcfb97eec8507
jhuleatt <jhuleatt@google.com>
2021-01-14 12:15:35 -0800

    add script

---
+++ generate-experimental-readme.js
@@ -0,0 +1,72 @@
+const { exec } = require('child_process');
+const { promisify } = require('util');
+const fetch = require('node-fetch');
+const prettier = require('prettier');
+
+const EXTENSIONS_REGISTRY_PROD =
+  'https://extensions-registry.firebaseapp.com/extensions.json';
+const EXTENSIONS_REGISTRY_STAGING =
+  'https://staging-extensions-registry.firebaseapp.com/extensions.json';
+
+function getLatestSource(extensionName) {
+  return fetch(EXTENSIONS_REGISTRY_STAGING)
+    .then((res) => res.json())
+    .then((registry) => {
+      const extensionMetadata = registry.mods[extensionName];
+
+      if (!extensionMetadata) {
+        throw new Error(
+          `Could not find extension "${extensionName}" in registry`
+        );
+      }
+
+      const latestVersion = extensionMetadata.labels.latest;
+      return extensionMetadata.versions[latestVersion];
+    });
+}
+
+function getExperimentalBlurb(extensionName, source) {
+  return `
+
+---
+
+## 🧩 Install this experimental extension
+
+> ⚠️ **Experimental**: This extension is available for testing as an _experimental_ release. It has not been as thoroughly tested as the officially released extensions, and future updates might introduce breaking changes. If you use this extension, please [report bugs and make feature requests](https://github.com/firebase/experimental-extensions/issues/new/choose) in our GitHub repository.
+
+### Console
+
+Use **[this install link](https://console.firebase.google.com/project/_/extensions/install?sourceName=${source})** to install this extension on your Firebase project!
+
+### Firebase CLI
+
+\`\`\`bash
+firebase ext:install ${extensionName} --project=<your-project-id>
+\`\`\`
+
+---
+
+`;
+}
+
+function runDefaultReadmeScript() {
+  return promisify(exec)('firebase ext:info .. --markdown').then((result) => {
+    return result.stdout;
+  });
+}
+
+async function generateReadme(extensionName) {
+  const initialReadme = await runDefaultReadmeScript();
+  const insertIndex = initialReadme.indexOf('**Details**:');
+
+  const extensionSource = await getLatestSource(extensionName);
+
+  const fullReadme =
+    initialReadme.slice(0, insertIndex) +
+    getExperimentalBlurb(extensionName, extensionSource) +
+    initialReadme.slice(insertIndex);
+
+  return prettier.format(fullReadme, { parser: 'markdown' });
+}
+
+generateReadme(process.argv[2]).then(console.log);
