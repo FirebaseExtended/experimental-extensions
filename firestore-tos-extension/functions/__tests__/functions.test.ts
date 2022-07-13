f56407f4727b6f1138a8089e24fb655d70e9ba67
dackers86 <ackers86@hotmail.com>
2022-07-13 18:35:46 +0100

    added test cases and initial functions

---
+++ firestore-tos-extension/functions/__tests__/functions.test.ts
@@ -0,0 +1,222 @@
+import setupEnvironment from "./helpers/setupEnvironment";
+setupEnvironment();
+
+import * as admin from "firebase-admin";
+const fft = require("firebase-functions-test")();
+
+import { TermsOfServiceAcceptance } from "../src/interface";
+import * as funcs from "../src/index";
+
+if (admin.apps.length === 0) {
+  admin.initializeApp({ projectId: "demo-test" });
+}
+
+const auth = admin.auth();
+
+/** prepare extension functions */
+const acceptTermsFn = fft.wrap(funcs.acceptTerms);
+const createTermsFn = fft.wrap(funcs.createTerms);
+const getTermsFn = fft.wrap(funcs.getTerms);
+
+describe("functions testing", () => {
+  describe("accept terms", () => {
+    describe("with a valid terms of service available", () => {
+      let user;
+      let tos_id;
+
+      beforeEach(async () => {
+        /** create example user */
+        user = await auth.createUser({});
+        tos_id = "tos_v1";
+        await createTermsFn.call(
+          {},
+          {
+            link: "www.link.to.terms",
+            tos_id,
+            creationDate: new Date().toLocaleDateString(),
+          },
+          { auth: { uid: user.uid } }
+        );
+      });
+
+      test("can add a terms of service", async () => {
+        await acceptTermsFn.call({}, { tos_id }, { auth: { uid: user.uid } });
+
+        const userRecord = await auth.getUser(user.uid);
+
+        expect(userRecord).toBeDefined();
+        expect(userRecord?.customClaims).toBeDefined();
+
+        const terms = userRecord?.customClaims[process.env.EXT_INSTANCE_ID];
+        expect(terms).toBeDefined();
+        expect(terms?.tos_acceptances).toBeDefined();
+
+        const acceptances: TermsOfServiceAcceptance = terms.tos_acceptances[0];
+
+        expect(acceptances).toBeDefined();
+        expect(acceptances.tosId).toBeDefined();
+        expect(acceptances.creationDate).toBeDefined();
+        expect(acceptances.acceptanceDate).toBeDefined();
+        expect(acceptances.customAttributes).toBeDefined();
+        expect(acceptances.customAttributes?.role).toBeDefined();
+      });
+
+      test("can add multiple terms of service agreements", async () => {
+        await acceptTermsFn.call({}, { tos_id }, { auth: { uid: user.uid } });
+        await acceptTermsFn.call({}, { tos_id }, { auth: { uid: user.uid } });
+
+        const userRecord = await auth.getUser(user.uid);
+
+        const terms = userRecord?.customClaims[process.env.EXT_INSTANCE_ID];
+        const acceptances: TermsOfServiceAcceptance = terms.tos_acceptances;
+
+        expect(acceptances).toHaveLength(2);
+      });
+    });
+
+    describe("without a valid user", () => {
+      let user;
+      let tos_id;
+
+      beforeEach(async () => {
+        /** create example user */
+        user = await auth.createUser({});
+        tos_id = "tos_v1";
+      });
+
+      test("does not add a terms of service", async () => {
+        await acceptTermsFn.call({}, { tos_id }, {});
+
+        const userRecord = await auth.getUser(user.uid);
+
+        const claims = userRecord?.customClaims;
+
+        expect(claims).toBeUndefined();
+      });
+    });
+
+    describe("Without a valid terms of service agreement", () => {
+      let user;
+
+      beforeEach(async () => {
+        /** create example user */
+        user = await auth.createUser({});
+      });
+
+      test("does not add a terms of service without a provided tos_id", async () => {
+        await acceptTermsFn.call({}, {}, { auth: { uid: user.uid } });
+
+        const userRecord = await auth.getUser(user.uid);
+
+        const claims = userRecord?.customClaims;
+
+        expect(claims).toBeUndefined();
+      });
+
+      test("does not add a terms of service without a exisiting tos_id", async () => {
+        await acceptTermsFn.call(
+          {},
+          { tos_id: "unknown" },
+          { auth: { uid: user.uid } }
+        );
+
+        const userRecord = await auth.getUser(user.uid);
+
+        const claims = userRecord?.customClaims;
+
+        expect(claims).toBeUndefined();
+      });
+    });
+  });
+
+  describe("get terms", () => {
+    let user;
+    let tos_id;
+
+    beforeEach(async () => {
+      /** create example user */
+      user = await auth.createUser({});
+
+      const randomId = Math.random().toString(36).substring(2, 15);
+      tos_id = `tos_v${randomId}`;
+    });
+
+    test("can get a terms of service", async () => {
+      await createTermsFn.call(
+        {},
+        {
+          link: "www.link.to.terms",
+          tos_id,
+          creationDate: new Date().toLocaleDateString(),
+        },
+        { auth: { uid: "test" } }
+      );
+
+      const terms = await getTermsFn.call(
+        {},
+        { tos_id },
+        { auth: { uid: user.uid } }
+      );
+
+      expect(terms).toBeDefined();
+      expect(terms?.link).toBeDefined();
+      expect(terms?.tos_id).toBeDefined();
+      expect(terms?.creationDate).toBeDefined();
+    });
+
+    test("can get a terms of service by tos_id", async () => {
+      await createTermsFn.call(
+        {},
+        {
+          link: "www.link.to.terms",
+          tos_id,
+          creationDate: new Date().toLocaleDateString(),
+        },
+        { auth: { uid: "test" } }
+      );
+
+      const terms = await getTermsFn.call(
+        {},
+        { tos_id },
+        { auth: { uid: user.uid } }
+      );
+
+      expect(terms).toBeDefined();
+      expect(terms?.link).toBeDefined();
+      expect(terms?.tos_id).toBeDefined();
+      expect(terms?.creationDate).toBeDefined();
+    });
+  });
+
+  describe("create terms", () => {
+    let tos_id;
+
+    beforeEach(async () => {
+      const randomId = Math.random().toString(36).substring(2, 15);
+      tos_id = `tos_v${randomId}`;
+    });
+    test("can create a terms of service", async () => {
+      const link = "www.link.to.terms";
+      const creationDate = new Date().toLocaleDateString();
+
+      await createTermsFn.call(
+        {},
+        { tos_id, link, creationDate },
+        { auth: { uid: "test" } }
+      );
+
+      const terms = await admin
+        .firestore()
+        .collection("terms")
+        .doc("agreements")
+        .collection("tos")
+        .doc(tos_id)
+        .get()
+        .then((doc) => doc.data());
+
+      expect(terms.tos_id).toEqual(tos_id);
+      expect(terms.creationDate).toEqual(creationDate);
+      expect(terms.link).toEqual(link);
+    });
+  });
+});
--- firestore-tos-extension/functions/src/index.ts
+++ firestore-tos-extension/functions/src/index.ts
@@ -8,33 +8,111 @@ if (admin.apps.length === 0) {
 }
 
 const auth = admin.auth();
+const db = admin.firestore();
 
-export const acceptTerms = functions.handler.https.onCall((data, context) => {
-  // Checking that the user is authenticated.
-  if (!context.auth) {
-    return;
-  }
+export const acceptTerms = functions.handler.https.onCall(
+  async (data, context) => {
+    // Checking that the user is authenticated.
+    if (!context.auth) {
+      return;
+    }
 
-  console.log("test 2 >>>>", process.env);
+    /** check if tos_id has been provided  */
+    if (!data.tos_id) {
+      console.warn("No tos_id provided");
+      return;
+    }
 
-  const claims = {};
-  claims[`${process.env.EXT_INSTANCE_ID}`] = {
-    tos_acceptances: [
-      {
-        tos_id: "publisher_tos_v1",
-        creation_date: "2021/02/23",
-        acceptance_date: "2022/02/23",
-        custom_attributes: {
-          role: "consumer",
+    /** check tos documents */
+    const tosDoc = await db
+      .collection(config.collectionPath)
+      .doc("agreements")
+      .collection("tos")
+      .doc(data.tos_id)
+      .get();
+
+    /** Return if no agreement exists  */
+    if (!tosDoc || !tosDoc.exists) {
+      console.warn("tosDoc does not exist");
+      return;
+    }
+
+    const tos_acceptances = await auth
+      .getUser(context.auth.uid)
+      .then(async (user) => {
+        if (!user.customClaims) return [];
+        return (
+          user.customClaims[`${process.env.EXT_INSTANCE_ID}`]
+            ?.tos_acceptances || []
+        );
+      });
+
+    const claims = {};
+    claims[`${process.env.EXT_INSTANCE_ID}`] = {
+      tos_acceptances: [
+        ...tos_acceptances,
+        {
+          tosId: "publisher_tos_v1",
+          creationDate: new Date().toLocaleDateString(),
+          acceptanceDate: new Date().toLocaleDateString(),
+          customAttributes: {
+            role: "consumer",
+          },
         },
-      },
-      {
-        tos_id: "publisher_tos_v2",
-        creation_date: "2021/02/24",
-        acceptance_date: "2022/02/23",
-      },
-    ],
-  };
+      ],
+    };
 
-  return admin.auth().setCustomUserClaims(context.auth.uid, claims);
-});
+    /** Add Acceptance cliam to user document list */
+    await db
+      .collection(config.collectionPath)
+      .doc("acceptances")
+      .collection(context.auth.uid)
+      .add({ claims });
+
+    /** Set claims on user and return */
+    return auth.setCustomUserClaims(context.auth.uid, claims);
+  }
+);
+
+export const createTerms = functions.handler.https.onCall(
+  async (data, context) => {
+    // Checking that the user is authenticated.
+    if (!context.auth) {
+      return;
+    }
+
+    /** check if tos data has been provided  */
+    if (!data) {
+      return;
+    }
+
+    /** Set claims on user and return */
+    return db
+      .collection(config.collectionPath)
+      .doc("agreements")
+      .collection("tos")
+      .doc(data.tos_id)
+      .set(data);
+  }
+);
+
+export const getTerms = functions.handler.https.onCall(
+  async (data, context) => {
+    // Checking that the user is authenticated.
+    if (!context.auth) {
+      return;
+    }
+
+    const { tos_id, latest_only = false } = data;
+
+    const query = db
+      .collection(config.collectionPath)
+      .doc("agreements")
+      .collection("tos");
+
+    if (latest_only) query.orderBy("creationDate", "desc").limit(1);
+    if (tos_id) query.where("tos_id", "==", tos_id);
+
+    return query.get().then((doc) => doc.docs[0].data());
+  }
+);
