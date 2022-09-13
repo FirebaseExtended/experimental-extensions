aa58e7e338cadf362ae557e5de6de79ffec220e1
dackers86 <ackers86@hotmail.com>
2022-09-13 13:39:34 +0100

    feat(firegraphql): initial commit

---
+++ firegraphql-extension/PREINSTALL.md
@@ -0,0 +1,8 @@
+
+
+#### Additional setup
+
+
+#### Billing
+
+
---
+++ firegraphql-extension/functions/src/index.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+// import * as logs from "./logs";
+import config from "./config";
+
+admin.initializeApp();
+
+export const handler = functions.handler.https.onCall(async (data, context) => {
+  // Checking that the user is authenticated.
+  if (!context.auth) {
+    throw new functions.https.HttpsError(
+      "unauthenticated",
+      "No valid authentication token provided."
+    );
+  }
+
+  return null;
+});
