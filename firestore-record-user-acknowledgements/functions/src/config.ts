db3fd617d5d489ecc92a439ffd6c15a1c2e22b7b
dackers86 <ackers86@hotmail.com>
2022-10-14 16:53:22 +0100

    chore: remove record user acknolwdgements extension

--- firestore-record-user-acknowledgements/functions/src/config.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  location: process.env.LOCATION,
-  projectId: process.env.PROJECT_ID,
-  noticesCollection: process.env.NOTICES_COLLECTION,
-  acknowledgementsCollection: process.env.ACKNOWLEDGEMENTS_COLLECTION,
-};
