8607bd34c3fe7e94c7f68a441a75fd7245580a90
Elliot Hesp <elliot.hesp@gmail.com>
2022-11-14 16:16:33 +0000

    feat: add basic flow

--- firestore-dialogflow-fulfillment/functions/src/index.ts
+++ firestore-dialogflow-fulfillment/functions/src/index.ts
@@ -17,8 +17,100 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import DialogFlow from "@google-cloud/dialogflow";
-import config from "./config";
+// import config from "./config";
 
-const flow = DialogFlow.v2beta1;
+const dialogflow = DialogFlow.v2beta1;
 
 admin.initializeApp();
+
+// exports.dialogflowFulfillment = functions.https.onRequest(
+//   async (request, response) => {
+//     if (request.method !== "POST") {
+//       response.status(405).end("Method Not Allowed");
+//       return;
+//     }
+
+//     // TODO: Handle auth: https://cloud.google.com/dialogflow/es/docs/fulfillment-webhook#auth
+
+//     const body = request.body;
+
+//     response.status(200);
+//   }
+// );
+
+exports.onInstall = functions.tasks.taskQueue().onDispatch(onInstall);
+
+async function onInstall() {
+  const agent = new dialogflow.AgentsClient({
+    projectId: "extensions-testing",
+  });
+
+  const [res] = await agent.setAgent({
+    agent: {
+      parent: `projects/extensions-testing/locations/global`,
+      displayName: "Test",
+      timeZone: "America/Los_Angeles", // TODO: Get from user
+    },
+  });
+
+  console.log(res);
+
+  const intentsClient = new dialogflow.IntentsClient();
+
+  const agentPath = intentsClient.projectAgentPath("extensions-testing");
+
+  await intentsClient.createIntent({
+    parent: agentPath,
+    intent: {
+      displayName: "TODO DISPLAY NAME",
+      messages: [
+        {
+          text: {
+            text: ["You are all set for $date at $time. See you then!"],
+          },
+        },
+      ],
+      parameters: [
+        {
+          displayName: "DATE",
+          entityTypeDisplayName: "@sys.date",
+          mandatory: true,
+          value: "$date",
+          prompts: ["What date would you like to schedule the appointment?"],
+        },
+        {
+          displayName: "TIME",
+          entityTypeDisplayName: "@sys.time",
+          mandatory: true,
+          value: "$time",
+          prompts: ["What time would you like to schedule the appointment?"],
+        },
+      ],
+      trainingPhrases: [
+        {
+          type: "EXAMPLE",
+          parts: [
+            {
+              text: "Set an appointment on ",
+            },
+            {
+              text: "Wednesday",
+              entityType: "@sys.date",
+            },
+            {
+              text: " at ",
+            },
+            {
+              text: "2 PM",
+              entityType: "@sys.time",
+            },
+          ],
+        },
+      ],
+    },
+  });
+
+  // TODO
+}
+
+onInstall();
