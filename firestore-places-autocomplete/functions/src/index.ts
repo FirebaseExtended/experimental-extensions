d42ec97def4877fb3613628b49e3463660f18185
Mais <mais.alheraki@gmail.com>
2023-03-07 00:50:06 +0300

    feat: initial commit

---
+++ firestore-places-autocomplete/functions/src/index.ts
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as functions from "firebase-functions";
+import axios from "axios";
+import config from "./config";
+
+export const autocomplete = functions.firestore
+	.document(`${config.collectionId}/{documentId}`)
+	.onWrite(async (snapshot) => {
+		const data = snapshot.after.data();
+
+		if (!data) return;
+
+		if (snapshot.after.isEqual(snapshot.before)) return;
+
+		const input = data.input;
+
+		const req = axios({
+			method: "get",
+			url: `https://maps.googleapis.com/maps/api/place/autocomplete/json`,
+			headers: {},
+			params: {
+				input: input,
+				types: "geocode",
+				language: "ar",
+				key: config.apiKey,
+			},
+		});
+
+		try {
+			const res = await req;
+			const predictions = res.data.predictions;
+			await snapshot.after.ref.update({ predictions: predictions });
+		} catch (err) {
+			functions.logger.error(err);
+		}
+	});
