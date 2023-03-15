386d6aadbbb92d4774b573f04b3933b29d6a3b8c
Mais <mais.alheraki@gmail.com>
2023-03-15 21:22:54 +0300

    feat: eventarc

--- api-keys-diagnostic/functions/src/index.ts
+++ api-keys-diagnostic/functions/src/index.ts
@@ -1,19 +1,54 @@
 import * as functions from "firebase-functions";
-import { apikeys_v2 } from "googleapis";
+import { apikeys_v2, google } from "googleapis";
 import * as admin from "firebase-admin";
+import { getEventarc } from "firebase-admin/eventarc";
 
 admin.initializeApp();
 
+const eventChannel =
+	process.env.EVENTARC_CHANNEL &&
+	getEventarc().channel(process.env.EVENTARC_CHANNEL);
+
 export const apiKeysDiagnostic = functions.pubsub
 	.schedule("every 2 minutes")
-	.onRun(async (context) => {
-		console.log("hi");
+	.onRun(async () => {
+		try {
+			const auth = new google.auth.GoogleAuth({
+				scopes: "https://www.googleapis.com/auth/cloud-platform",
+			});
 
-		// const client = new apikeys_v2.Apikeys({});
+			const authClient = await auth.getClient();
 
-		// const { data } = await client.projects.locations.keys.list({
-		// 	parent: `projects/${functions.config().project.id}`,
-		// });
+			const client = new apikeys_v2.Apikeys({
+				auth: authClient,
+			});
 
-		// console.log(data);
+			const { data } = await client.projects.locations.keys.list({
+				parent: `projects/dev-extensions-testing/locations/global`,
+			});
+
+			if (!data.keys) return;
+
+			// Check if any of the API keys are not restricted
+			const unsecuredKeys = data.keys.filter((key) => !key.restrictions);
+			console.log(JSON.stringify(unsecuredKeys, null, 2));
+
+			// If there are any unsecured keys, send an email notification
+			if (unsecuredKeys.length > 0) {
+				eventChannel &&
+					eventChannel.publish({
+						type: "firebase.extensions.api-keys-diagnostic.unsecured",
+						subject: "Unsecured API Keys Found",
+						source: "firebase",
+						data: {
+							message: `The following API keys have no restrictions:\n\n${unsecuredKeys
+								.map((key) => key.name)
+								.join("\n")}`,
+							keys: JSON.stringify(unsecuredKeys),
+						},
+					});
+			}
+		} catch (error) {
+			console.error("Error: ", error);
+		}
 	});
