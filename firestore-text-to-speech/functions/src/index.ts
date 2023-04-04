a58281d7bc182f2ca0f6f089fb7d273f6c833ce4
Jacob Cable <jacobcable94@gmail.com>
2023-04-04 12:03:16 +0100

    refactor(firestore-text-to-speech): remove task queueing

--- firestore-text-to-speech/functions/src/index.ts
+++ firestore-text-to-speech/functions/src/index.ts
@@ -1,9 +1,9 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
-import { getFunctions } from "firebase-admin/functions";
 import config from "./config";
 import * as tts from "@google-cloud/text-to-speech";
-import { AudioEncoding, ISynthesizeSpeechRequest, ISynthesizeSpeechResponse, SsmlVoiceGender } from "./types";
+import { ISynthesizeSpeechRequest, ISynthesizeSpeechResponse } from "./types";
+import { buildRequest, BuildRequestOptions, getFileExtension } from "./util";
 
 const logger = functions.logger;
 
@@ -11,68 +11,7 @@ admin.initializeApp();
 
 const ttsClient = new tts.TextToSpeechClient();
 
-
-interface BuildRequestOptions {
-    text: string;
-    languageCode?: string;
-    ssmlGender?: SsmlVoiceGender;
-    audioEncoding?: AudioEncoding;
-    voiceName?: string;
-}
-
-function buildRequest({
-    text,
-    languageCode = config.languageCode,
-    ssmlGender = config.ssmlGender,
-    audioEncoding = config.audioEncoding,
-    voiceName = config.voiceName
-}: BuildRequestOptions): ISynthesizeSpeechRequest {
-    return {
-        input: config.ssml ? { ssml: text } : { text: text },
-        voice: voiceName ? { name: voiceName } : {languageCode, ssmlGender},
-        audioConfig: {
-            audioEncoding
-        }
-    }
-}
-
-
-export const processText = functions
-    .runWith({ secrets: [`ext-${process.env.EXT_INSTANCE_ID}-API_KEY`] })
-    .tasks.taskQueue({
-        retryConfig: {
-            maxAttempts: 3,
-            minBackoffSeconds: 60,
-            maxDoublings: 6,
-        },
-        rateLimits: {
-            maxConcurrentDispatches: 6,
-        },
-    })
-    .onDispatch(async (data) => {
-        const request: ISynthesizeSpeechRequest = data.request;
-        const docId: string = data.docId;
-
-        try {
-            const speech = await textToSpeech(request);
-            if (speech && speech.audioContent) {
-                // Merge the address validity data with the address document.
-
-                const bucket = admin.storage().bucket();
-                const file = bucket.file(docId + ".mpeg");
-
-                await file.save(Buffer.from(speech.audioContent))
-
-                return;
-            }
-        } catch (error) {
-
-            throw error;
-        }
-    });
-
-
-export const textToSpeechTrigger = functions.firestore
+export const textToSpeech = functions.firestore
     .document(`${config.collectionPath}/{docId}`)
     .onCreate(async (snap, _) => {
         if (snap.data().text) {
@@ -91,38 +30,36 @@ export const textToSpeechTrigger = functions.firestore
                 audioEncoding,
                 voiceName
             }) : buildRequest({ text });
-            await enqueueTask(request, snap.id);
+
+            try {
+                const speech = await processText(request);
+                if (speech && speech.audioContent) {
+                    // Merge the address validity data with the address document.
+                    const fileExtension = getFileExtension(audioEncoding);
+
+                    const fileName = config.storagePath ? `${config.storagePath}/${snap.id}${fileExtension}` : `${snap.id}${fileExtension}`;
+
+                    const bucket = admin.storage().bucket(config.bucketName);
+                    const file = bucket.file(fileName);
+                    await file.save(Buffer.from(speech.audioContent));
+
+                    return;
+                }
+            } catch (error) {
+                throw error;
+            }
         }
         return;
     });
 
-
-
-async function enqueueTask(request: ISynthesizeSpeechRequest, docId: string) {
-    // Retry the request if it fails.
-    const queue = getFunctions().taskQueue(
-        `ext-${process.env.EXT_INSTANCE_ID}-processText`
-    );
-    await queue.enqueue(
-        {
-            request,
-            docId,
-        },
-        // Retry the request after 60 seconds.
-        { scheduleDelaySeconds: 60 }
-    );
-}
-
-
-async function textToSpeech(request: ISynthesizeSpeechRequest) {
+async function processText(request: ISynthesizeSpeechRequest) {
     let response: ISynthesizeSpeechResponse;
     // Performs the text-to-speech request
     try {
-
         [response] = await ttsClient.synthesizeSpeech(request);
     } catch (e) {
-        logger.warn(e);
+        logger.error(e);
+        throw e;
     }
-    logger.log(JSON.stringify(response));
     return response;
 };
---
+++ firestore-text-to-speech/functions/src/util.ts
@@ -0,0 +1,44 @@
+import { AudioEncoding, ISynthesizeSpeechRequest, ISynthesizeSpeechResponse, SsmlVoiceGender } from "./types";
+import config from "./config";
+
+export interface BuildRequestOptions {
+    text: string;
+    languageCode?: string;
+    ssmlGender?: SsmlVoiceGender;
+    audioEncoding?: AudioEncoding;
+    voiceName?: string;
+}
+
+export function buildRequest({
+    text,
+    languageCode = config.languageCode,
+    ssmlGender = config.ssmlGender,
+    audioEncoding = config.audioEncoding,
+    voiceName = config.voiceName
+}: BuildRequestOptions): ISynthesizeSpeechRequest {
+    return {
+        input: config.ssml ? { ssml: text } : { text: text },
+        voice: voiceName ? { name: voiceName } : {languageCode, ssmlGender},
+        audioConfig: {
+            audioEncoding
+        }
+    }
+}
+
+
+export function getFileExtension(audioEncoding: AudioEncoding): string {
+    switch (audioEncoding) {
+        case "LINEAR16":
+            return ".wav";
+        case "MP3":
+            return ".mp3";
+        case "OGG_OPUS":
+            return ".ogg";
+        case "MULAW":
+            return ".mulaw";
+        case "ALAW":
+            return ".alaw";
+        default:
+            return ".wav";
+    }
+}
