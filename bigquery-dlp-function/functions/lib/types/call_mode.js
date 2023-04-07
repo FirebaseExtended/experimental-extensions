4dbf8930f6f45f7835da35be823bd16d091db88e
Darren Ackers <ackers86@hotmail.com>
2023-04-07 17:49:51 +0100

    chore: added gitignore

--- bigquery-dlp-function/functions/lib/types/call_mode.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var CallMode;
-(function (CallMode) {
-    CallMode["DEIDENTIFY"] = "DEIDENTIFY";
-    CallMode["REIDENTIFY"] = "REIDENTIFY";
-})(CallMode || (CallMode = {}));
-exports.default = CallMode;
-//# sourceMappingURL=call_mode.js.map
