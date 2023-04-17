9bd31b0a009434914f12d954c2cfecac80ee72c2
Darren Ackers <ackers86@hotmail.com>
2023-04-17 15:43:34 +0100

    chore: ran format, include testing env file

--- storage-image-labeling/functions/src/types.ts
+++ storage-image-labeling/functions/src/types.ts
@@ -1,18 +1,21 @@
-import * as vision from '@google-cloud/vision';
+import * as vision from "@google-cloud/vision";
 export type VisionClient = vision.v1.ImageAnnotatorClient;
 // this isn't exported from the sdk, and is different from IAnnotateImageRequest, but is used in the annotateImage method
 export type ImprovedRequest = {
-    image?: {
-        source?: {
-            filename?: string;
-            imageUri?: string;
-        };
-        content?: Uint8Array | string | null;
+  image?: {
+    source?: {
+      filename?: string;
+      imageUri?: string;
     };
-    features?: any;
-    imageContext?: any;
-}
-export type IAnnotatedImageRequest = vision.protos.google.cloud.vision.v1.IAnnotateImageRequest;
+    content?: Uint8Array | string | null;
+  };
+  features?: any;
+  imageContext?: any;
+};
+export type IAnnotatedImageRequest =
+  vision.protos.google.cloud.vision.v1.IAnnotateImageRequest;
 export type ImageContext = vision.protos.google.cloud.vision.v1.IImageContext;
-export type IAnnotatedImageResponse = vision.protos.google.cloud.vision.v1.IAnnotateImageResponse;
-export type IEntityAnnotation = vision.protos.google.cloud.vision.v1.IEntityAnnotation;
+export type IAnnotatedImageResponse =
+  vision.protos.google.cloud.vision.v1.IAnnotateImageResponse;
+export type IEntityAnnotation =
+  vision.protos.google.cloud.vision.v1.IEntityAnnotation;
