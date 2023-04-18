8fa28779bcd1ec63eaf3586fbb7f551bce7cce54
Darren Ackers <ackers86@hotmail.com>
2023-04-18 09:59:40 +0100

    chore: ran formatting

--- firestore-schedule-writes/functions/src/config.ts
+++ firestore-schedule-writes/functions/src/config.ts
@@ -1,13 +1,11 @@
-
-
 export default {
-    schedule: process.env.SCHEDULE || 'every 1 minutes',
-    mergeWrite: process.env.MERGE_WRITE === "true",
-    queueCollection: process.env.QUEUE_COLLECTION || "queued_writes",
-    targetCollection: process.env.TARGET_COLLECTION,
-    stalenessThresholdSeconds: parseInt(
-        process.env.STALENESS_THRESHOLD_SECONDS || "0",
-        10
-    ),
-    cleanup: (process.env.CLEANUP as "DELETE" | "KEEP") || "DELETE"
-}
+  schedule: process.env.SCHEDULE || "every 1 minutes",
+  mergeWrite: process.env.MERGE_WRITE === "true",
+  queueCollection: process.env.QUEUE_COLLECTION || "queued_writes",
+  targetCollection: process.env.TARGET_COLLECTION,
+  stalenessThresholdSeconds: parseInt(
+    process.env.STALENESS_THRESHOLD_SECONDS || "0",
+    10
+  ),
+  cleanup: (process.env.CLEANUP as "DELETE" | "KEEP") || "DELETE",
+};
